0fdb7db3ba0a6a35a40c1c469c0fbff6e69134c0
Mehran Granfar <mgranfar@gmail.com>
2026-01-27 17:25:46 +0400

    post npm i + publish push

--- dist/types/metrics-calcs.d.ts
+++
@@ -1,6 +0,0 @@
-import { Bar, BenchmarkBar } from "./types/alpaca-types";
-import { types } from "@adaptic/backend-legacy";
-import { CalculateBetaResult, TradeMetrics } from "./types";
-export declare function calculateBetaFromReturns(portfolioReturns: number[], benchmarkReturns: number[]): CalculateBetaResult;
-export default function fetchTradeMetrics(trade: types.Trade, tradeBars: Bar[], benchmarkBars: BenchmarkBar[]): Promise<TradeMetrics>;
-//# sourceMappingURL=metrics-calcs.d.ts.map
--- dist/types/testing/options-ws.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=options-ws.d.ts.map
